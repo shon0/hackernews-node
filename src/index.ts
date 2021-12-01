996ecb32aa88bc1285a4a74574df4ad5cf60cb5c
shon0 <shono.koga@gmail.com>
2021-12-01 23:42:40 +0900

    Create graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-const HELLO = 'hello'
+import { ApolloServer } from 'apollo-server'
+import fs from 'fs'
+import path from 'path'
 
-console.log(HELLO)
+const links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+]
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (_: any, args: any) => {
+      const filteredLinks = links.filter(link => link.id === args.id)
+      return filteredLinks.length > 0 ? filteredLinks[0] : null
+    },
+  },
+  Mutation: {
+    post: (_: any, args: any) => {
+      let idCount = links.length
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
+  resolvers,
+})
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`))
