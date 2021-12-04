26550c33ace1f05706c7b5a18ac539a72f6cabf6
shon0 <shono.koga@gmail.com>
2021-12-04 21:55:34 +0900

    Updating the resolver functions to use Prisma Client

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,36 @@
+import { PrismaClient } from '@prisma/client'
 import { ApolloServer } from 'apollo-server'
 import fs from 'fs'
 import path from 'path'
 
-const links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL',
-  },
-]
-
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (_: any, args: any) => {
-      const filteredLinks = links.filter(link => link.id === args.id)
-      return filteredLinks.length > 0 ? filteredLinks[0] : null
+    feed: async (parent: any, args: any, context: any) => {
+      return context.prisma.link.findMany()
     },
   },
   Mutation: {
-    post: (_: any, args: any) => {
-      let idCount = links.length
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+    post: (parent: any, args: any, context: any, info: any) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
     },
   },
 }
 
+const prisma = new PrismaClient()
+
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
+  context: {
+    prisma,
+  },
 })
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`))
