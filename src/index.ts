b0337d4c6755679774df013f153c014845a0d0a1
shon0 <shono.koga@gmail.com>
2021-12-11 20:35:02 +0900

    migrate apollo-server-express

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,44 @@
 import { PrismaClient } from '@prisma/client'
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import express from 'express'
+import http from 'http'
 import { loadSchemaSync } from '@graphql-tools/load'
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
-import { addResolversToSchema } from '@graphql-tools/schema'
+import { addResolversToSchema, makeExecutableSchema } from '@graphql-tools/schema'
 import { join } from 'path'
 import { getUserId } from './utils'
 import { resolvers } from './resolvers'
 
-const schema = loadSchemaSync(join(__dirname, './schema.graphql'), {
-  loaders: [new GraphQLFileLoader()],
-})
+async function startApolloServer() {
+  const schema = loadSchemaSync(join(__dirname, './schema.graphql'), {
+    loaders: [new GraphQLFileLoader()],
+  })
+  const schemaWithResolvers = addResolversToSchema({ schema, resolvers })
 
-const prisma = new PrismaClient()
+  const app = express()
+  const httpServer = http.createServer(app)
 
-const schemaWithResolvers = addResolversToSchema({ schema, resolvers })
-const server = new ApolloServer({
-  schema: schemaWithResolvers,
-  context: ({ req }) => ({
-    ...req,
-    prisma,
-    userId: req && req.headers.authorization ? getUserId(req) : null,
-  }),
-})
+  const prisma = new PrismaClient()
 
-server.listen().then(({ url }) => console.log(`Server is running on ${url}`))
+  const server = new ApolloServer({
+    schema: schemaWithResolvers,
+    context: ({ req }) => ({
+      ...req,
+      prisma,
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+
+  await server.start()
+  server.applyMiddleware({
+    app,
+    path: '/',
+  })
+
+  await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve))
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+}
+
+startApolloServer()
